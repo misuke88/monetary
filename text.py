d60bd3e40e7d4453ec3e86c618c52f6ccdaae1ce
misuk <mskim@dm.snu.ac.kr>
2016-09-28 15:57:44 +0900

    Modify read files

--- text.py
+++ text.py
@@ -6,10 +6,21 @@ from settings import DATA_DIR
 
 #def read_files(filename):
 
+i =0 
 
 if __name__ == '__main__':
 	filename= '%s/INFO_CLASS.csv' % DATA_DIR
-	
-	with open(filename, 'r') as f:
-		for line in f.readlines():
-			print(line)
+	data = pd.read_csv(filename, sep=",", header= 0)
+	print(data)
+	for i in range(len(data)-1):
+		txtname = "%s.txt" % data[i][0]
+		with open(txtname, 'r') as f:
+			for line in f.readlines():
+				testfile.add(line)
+	print(len(data))
+	# with open(filename, 'r') as f:
+	# 	for line in f.readlines():
+	# 		print(len(line))
+	# 		i = i + 1
+			#print(line,i)
+	#len(filenames)
