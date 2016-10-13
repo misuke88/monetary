321c3e07be1b366db7a17d486db6954b5c6aed4d
misuk <mskim@dm.snu.ac.kr>
2016-10-13 15:59:25 +0900

    Add settings.py files

---
+++ settings.py
@@ -0,0 +1,8 @@
+#! /usr/bin/python
+# -*- coding: utf-8 -*-
+
+# fill me
+# DIR_8K      = '.'   # ex: F:/finance/8K.tar/8K-gz
+DATA_DIR    = './MonetaryPolicy/MonetaryPolicy_Text'
+LOG_DIR     = './logs'
+
